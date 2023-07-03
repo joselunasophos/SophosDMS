b5dfd62d21e30f5bd7916dc390ba9f00fe9ef496
joselunasophos <131683421+joselunasophos@users.noreply.github.com>
2023-07-03 13:23:39 -0500

    Update Deploy.sh

--- DmsTask/Deploy.sh
+++ DmsTask/Deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-stack_name="Dms--Task"
+current_branch=$(git branch --show-current) 
+stack_name="Dms-$current_branch-Task"
 
 account_id=$(aws sts get-caller-identity --query "Account" --output text)
 
 bucket="s3://bucket-dms-excel-$account_id/cf_file_out/taskparams.json"
 
-aws s3 cp "$bucket"
+aws s3 cp "$bucket" .
 
 SCRIPT_DIR=$(dirname "$0")
 template_file="$SCRIPT_DIR/template.yml"
